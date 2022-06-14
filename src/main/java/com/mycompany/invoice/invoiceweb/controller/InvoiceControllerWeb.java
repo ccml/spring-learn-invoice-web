0a0e57111a0c72c769108da9e47588e406685318
ccml <claude.cato@proximus.be>
2022-06-14 18:26:09 +0200

    valider formulaire

--- src/main/java/com/mycompany/invoice/invoiceweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoice/invoiceweb/controller/InvoiceControllerWeb.java
@@ -3,19 +3,22 @@ package com.mycompany.invoice.invoiceweb.controller;
 import com.mycompany.invoice.core.controller.InvoiceControllerInterface;
 import com.mycompany.invoice.core.entity.Invoice;
 import com.mycompany.invoice.core.service.InvoiceServiceInterface;
+import com.mycompany.invoice.invoiceweb.form.InvoiceForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequestMapping("/invoice")
-public class InvoiceControllerWeb implements InvoiceControllerInterface {
+public class InvoiceControllerWeb {
 
     @Autowired
     private InvoiceServiceInterface invoiceService;
@@ -29,9 +32,16 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
     }
 
     @PostMapping("")
-    public String createInvoice(@ModelAttribute Invoice invoice) {
-        invoiceService.createInvoice(invoice);
-        return "invoice-created";
+    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult results) {
+        if(results.hasErrors()) {
+            return "invoice-create-form";
+        } else {
+            Invoice invoice = new Invoice();
+            invoice.setCustomerName(invoiceForm.getCustomerName());
+            invoice.setOrderNumber(invoiceForm.getOrderNumber());
+            invoiceService.createInvoice(invoice);
+            return "invoice-created";
+        }
     }
 
     @GetMapping("/home")
@@ -51,7 +61,7 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
     }
 
     @GetMapping("/create-form")
-    public String displayInvoiceCreateForm(@ModelAttribute Invoice invoice) {
+    public String displayInvoiceCreateForm(@ModelAttribute InvoiceForm invoiceForm) {
         return "invoice-create-form";
     }
 }
